5631f0b3a6cc783c219bc6cf56237ed4a1b0438e
Vandana <Vandana@DESKTOP-0GITLAK>
2024-07-16 11:36:23 +0530

    Initial commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
-@OpenAPIDefinition(
+/*@OpenAPIDefinition(
 		info=@Info(
 		    title="tc",
 		    version="1.0.",
@@ -18,7 +18,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         		url="http://localhost:8080",
         		description="TRE"
         		)
-)
+)*/
 
 public class DemoApplication {
 
